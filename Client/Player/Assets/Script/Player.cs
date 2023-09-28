f1bb9dfa433152958de466c052bc94347de29a38
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2023-09-28 17:09:59 +0900

    Player Animation Test
    Test
    * Type of Riging
    * player avarta Mask
    * player SImple Move Test

---
+++ Client/Player/Assets/Script/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PlayerStat
+{
+	public float maxHp;
+	public float curHp;
+
+	public float maxStamina;
+	public float curStamina;
+
+	public float moveSpd;
+
+	public float upperRotSpd;
+	public float lowerRotSpd;
+}
+
+
+public class Player : MonoBehaviour
+{
+
+	public PlayerStat stat;
+
+
+
+	[Space(10f)]
+	public PlayerMove moveScript;
+	public PlayerAttack atkScript;
+
+
+	private void Awake()
+	{
+		if (!moveScript)
+		{ moveScript = GetComponent<PlayerMove>(); }
+	}
+
+	void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+		moveScript.Move();
+    }
+
+
+	private void OnEnable()
+	{
+		
+	}
+
+	private void OnDisable()
+	{
+		
+
+	}
+
+	
+}
