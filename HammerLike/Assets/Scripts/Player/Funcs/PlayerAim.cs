22b6bc869a8ef957af289f4277344bd101cbbb8e
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2023-10-16 08:30:08 +0900

    UpperBody Rotate Testing
    학교에서 테스트 해보기
    
    할거
    1. 움직일때 다리 애니메이션만 따로 움직이게 하는거

--- HammerLike/Assets/Scripts/Player/Funcs/PlayerAim.cs
+++ HammerLike/Assets/Scripts/Player/Funcs/PlayerAim.cs
@@ -9,9 +9,11 @@ public class PlayerAim : MonoBehaviour
 	Vector3 mouseWorldPos;
 	Vector3 playerToMouseDir;
 
+	Transform testCube;
+
 	private void Awake()
 	{
-		
+		testCube = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
 	}
 
 	public Vector3 Aiming(float mouseSpd = 1f)
@@ -21,16 +23,25 @@ public class PlayerAim : MonoBehaviour
 		//Debug.Log("MousePos : " + mousePos + "\nplayer Pos : " + playerScreenPos); 
 		//Vector2 dir = 
 
+		
 
 		Vector3 mouseScreenPos = Input.mousePosition;
 		Ray mouseRay = Camera.main.ScreenPointToRay(mouseScreenPos); 
+
 		RaycastHit hit;
-		if (Physics.Raycast(mouseRay, out hit,100f, LayerMask.GetMask("Ground"))) 
+		if (Physics.Raycast(mouseRay, out hit,500f, LayerMask.GetMask("Ground"))) 
 		{
+			
 			Debug.Log("dd");
 			mouseWorldPos= hit.point;
-			mouseWorldPos.y = transform.position.y;
+			//mouseWorldPos.y = transform.position.y;
 
+			//231016 0816 지금 이거 정확한 위치가 아님.
+			//아마 실질적인 작동 카메라랑 최종적 렌더링 카메라에서 나오는 단차인듯 함.
+			//다 스크린 좌표로 바꿔서 해보자
+			testCube.position = mouseWorldPos;
+
+			
 			playerToMouseDir = (mouseWorldPos - transform.position).normalized;
 		}
 
