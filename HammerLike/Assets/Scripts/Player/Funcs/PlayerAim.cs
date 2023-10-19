e50ec8947853b7cd8c9a2c0eb0f3934cbb7c11af
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2023-10-20 04:42:25 +0900

    code Clean up
    할거
    1. 움직일 때 다리 애니메이션 재생
    2. 상하체 움직임 추가적인 조건 처리 해주기
    3. 명진님 하고 플레이어 작업 방식 한번 얘기 해보기
    4. 공격 기능 추가(애미네이션 작동 정도 까지만)

--- HammerLike/Assets/Scripts/Player/Funcs/PlayerAim.cs
+++ HammerLike/Assets/Scripts/Player/Funcs/PlayerAim.cs
@@ -11,84 +11,30 @@ public class PlayerAim : MonoBehaviour
 	Vector3 mouseWorldPos;
 	Vector3 playerToMouseDir;
 
-
-
-	Transform testCube;
-
 	[SerializeField]
 	PixelPerfectCamera zoomPixelCam;
 	[SerializeField]
 	Camera zoomCam;
 
 	Ray mouseRay;
-
 	Vector3 rayResultPoint;
 
-	[SerializeField]
-	Transform renderPlane;
 
-	public RenderTexture renderTex;
+#if UNITY_EDITOR
+	Transform testCube;
+#endif
+
+	//[SerializeField]
+	//Transform renderPlane;
+	//public RenderTexture renderTex;
 
 	private void Awake()
 	{
+#if UNITY_EDITOR
 		testCube = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
 		testCube.transform.localScale *= 4f;
+#endif
 	}
-
-	//public Vector3 Aiming(float mouseSpd = 1f)
-	//{
-	//    //Vector2 mousePos = Input.mousePosition;
-	//    //Vector2 playerScreenPos = Camera.main.WorldToScreenPoint(transform.position);
-	//    //Debug.Log("MousePos : " + mousePos + "\nplayer Pos : " + playerScreenPos); 
-	//    //Vector2 dir = 
-
-
-
-	//    Vector3 mouseScreenPos = Input.mousePosition;
-
-
-	//    Vector2 mouseViewPos_main = Camera.main.ScreenToViewportPoint(mouseScreenPos);
-	//    Vector2 mouseViewPos_zoom = zoomCam.ScreenToViewportPoint(mouseScreenPos);
-	//    float zoomCamSize = zoomCam.orthographicSize;
-	//    float ratio = zoomCamSize / zoomPixelCam.maxCameraHalfHeight;
-
-	//    Debug.Log("main View : " + mouseViewPos_main + "\nZoom View : " + mouseViewPos_zoom);
-	//    //당연히 다른 값이 나옴...? 
-	//    //23.10.17 1602 왜 또 같노?
-	//    //이거 보니까 화면 해상도에 따라서 다름.
-
-
-	//    Ray mouseRay = Camera.main.ScreenPointToRay(mouseScreenPos);
-	//    //줌 카메라로 바꾼다면 레이 처리 자체가 안될꺼임.
-
-
-	//    RaycastHit hit;
-	//    if (Physics.Raycast(mouseRay, out hit, 500f, LayerMask.GetMask("Ground")))
-	//    {
-
-	//        Debug.Log("dd");
-	//        mouseWorldPos = hit.point;
-	//        //mouseWorldPos.y = transform.position.y;
-
-	//        //231016 0816 지금 이거 정확한 위치가 아님.
-	//        //아마 실질적인 작동 카메라랑 최종적 렌더링 카메라에서 나오는 단차인듯 함.
-	//        //둘 다 스크린 좌표로 바꿔서 계산해보기?
-	//        //231017 1413
-	//        //둘 다 스크린 좌표로 바꾸더라도 차이가 날것임. 결국에는 최종적 스크린 좌표로 나오는 카메라 자체가 다르니까.
-	//        //그렇다면 마우스 좌표를 보정해줘야 할 거 같은디
-
-	//        //스크린 좌표를 사용하는거 보다
-	//        //Zoom카메라의 줌 값에 따른 뷰포트의 차이를 보정해주고 그걸로 
-	//        //다시 레이를 쏴서 체크하는 방식이 어쩌면 더 정확할거 같음.
-	//        testCube.position = mouseWorldPos;
-
-
-	//        playerToMouseDir = (mouseWorldPos - transform.position).normalized;
-	//    }
-
-	//    return playerToMouseDir;
-	//}
-
 
 	public Vector3 Aiming(float mouseSpd = 1f)
 	{
@@ -156,8 +102,10 @@ public class PlayerAim : MonoBehaviour
 			//스크린 좌표를 사용하는거 보다
 			//Zoom카메라의 줌 값에 따른 뷰포트의 차이를 보정해주고 그걸로 
 			//다시 레이를 쏴서 체크하는 방식이 어쩌면 더 정확할거 같음.
+
+#if UNITY_EDITOR
 			testCube.position = mouseWorldPos;
-			
+#endif
 			rayResultPoint = mouseWorldPos;
 
 			playerToMouseDir = (mouseWorldPos - transform.position).normalized;
@@ -168,47 +116,6 @@ public class PlayerAim : MonoBehaviour
 		return playerToMouseDir;
 		
 	}
-
-
-	//public float Test(float val, float ratio)
-	//{
-	//	//float offset = ((ratio - 1f) * 0.5f) + 1;
-	//	//Vector2 clamp = new Vector2(0.5f - (1))
-
-	//	return val < 0.5f ? 0.5f -  val * ratio : 0.5f + val * ratio;
-	//}
-
-	//public Vector2 Test(Vector2 val, float ratio)
-	//{
-	//	//float offset = ((ratio - 1f) * 0.5f) + 1;
-	//	Vector2 temp = new Vector2(Test(val.x, ratio), Test(val.y, ratio));
-	//	return temp;
-
-	//	//Vector2 result = val;
-
-	//	//if (val.x > 0.5f && val.y > 0.5)
-	//	//{//1사분면
-	//	//	result.x = val.x * ratio;
-	//	//	result.y = val.y * ratio;
-	//	//}
-	//	//else if (val.x < 0.5f && val.y > 0.5f)
-	//	//{//2사분면
-	//	//	result.x = val.x * ratio;
-	//	//	result.y = val.y * ratio;
-	//	//}
-	//	//else if (val.x < 0.5f && val.y < 0.5f)
-	//	//{//3사분면
-	//	//	result.x = val.x * ratio;
-	//	//	result.y = val.y * ratio;
-	//	//}
-	//	//else if (val.x > 0.5f && val.y < 0.5f)
-	//	//{ //4사분면
-	//	//	result.x = val.x * ratio;
-	//	//	result.y = val.y * ratio;
-	//	//}
-
-	//	//return result;
-	//}
 
 
 	private void OnDrawGizmos()
--- HammerLike/Assets/Scripts/Player/Funcs/PlayerMove.cs
+++ HammerLike/Assets/Scripts/Player/Funcs/PlayerMove.cs
@@ -23,30 +23,4 @@ public class PlayerMove : MonoBehaviour
         
     }
 
-    //public float speed = 5f; // 플레이어의 이동 속도를 정의합니다.
-    //public Camera cam; // 메인 카메라를 할당하기 위한 변수입니다.
-
-    //void Update()
-    //{
-    //    float horizontal = Input.GetAxis("Horizontal"); // 수평 방향의 입력을 받습니다.
-    //    float vertical = Input.GetAxis("Vertical"); // 수직 방향의 입력을 받습니다.
-
-    //    Vector3 moveDirection = new Vector3(horizontal, 0f, vertical).normalized; // 입력값을 바탕으로 이동 방향을 결정합니다.
-
-    //    // 마우스 버튼을 누르고 있는 경우
-    //    if (Input.GetMouseButton(0))
-    //    {
-    //        Ray ray = cam.ScreenPointToRay(Input.mousePosition); // 마우스 위치로부터 Ray를 쏘아줍니다.
-    //        RaycastHit hit;
-
-    //        if (Physics.Raycast(ray, out hit)) // Ray와 물체가 충돌한 경우
-    //        {
-    //            Vector3 targetPosition = hit.point; // Ray가 물체와 충돌한 위치입니다.
-    //            targetPosition.y = transform.position.y; // y 좌표를 플레이어의 y 좌표로 변경합니다.
-    //            transform.LookAt(targetPosition); // 플레이어가 마우스 커서의 방향을 바라보게 합니다.
-    //        }
-    //    }
-
-    //    transform.Translate(moveDirection * speed * Time.deltaTime, Space.World); // 이동 방향과 속도를 바탕으로 플레이어를 이동시킵니다.
-    //}
 }
