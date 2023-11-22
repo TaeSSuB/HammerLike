b9cb38c63dd6ffdd90968bee7516c408da8d95bc
TaeSSuB <owwpww@gmail.com>
2023-11-22 11:59:46 +0900

    Background Scene

--- HammerLike/Assets/VolumetricLights/Resources/Shaders/Options.hlsl
+++ HammerLike/Assets/VolumetricLights/Resources/Shaders/Options.hlsl
@@ -2,7 +2,7 @@
 #define VOLUMETRIC_LIGHTS_OPTIONS
 
 /* Uncomment this line to fully support orthographic camera */
-//#define ORTHO_SUPPORT
+#define ORTHO_SUPPORT
 
 /* Uncomment this line to force use the old world reconstruction method (should be used for VR modes) */
 //#define USE_ALTERNATE_RECONSTRUCT_API
