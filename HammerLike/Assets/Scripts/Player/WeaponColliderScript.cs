927dfdece6e13063377acd820add8dcd5b68e6fe
GraphicsEngineer <jin48506143@gmail.com>
2024-02-15 20:50:45 +0900

    go
    go

--- HammerLike/Assets/Scripts/Player/WeaponColliderScript.cs
+++ HammerLike/Assets/Scripts/Player/WeaponColliderScript.cs
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
 using UnityEngine;
 
 public class WeaponColliderScript : MonoBehaviour
 {
-    public PlayerAtk playerAtk; // PlayerAtk ½ºÅ©¸³Æ®ÀÇ ÂüÁ¶
+    public PlayerAtk playerAtk; // PlayerAtk ?¤í¬ë¦½íŠ¸??ì°¸ì¡°
 
     private void Awake()
     {
-        // playerAtk ÄÄÆ÷³ÍÆ®¸¦ ÀÚµ¿À¸·Î Ã£¾Æ¼­ ÇÒ´çÇÒ ¼öµµ ÀÖ½À´Ï´Ù.
-        // ¿¹: playerAtk = FindObjectOfType<PlayerAtk>();
-=======
-ï»¿using UnityEngine;
-
-public class WeaponColliderScript : MonoBehaviour
-{
-    public PlayerAtk playerAtk; // PlayerAtk ìŠ¤í¬ë¦½íŠ¸ì˜ ì°¸ì¡°
-
-    private void Awake()
-    {
-        // playerAtk ì»´í¬ë„ŒíŠ¸ë¥¼ ìë™ìœ¼ë¡œ ì°¾ì•„ì„œ í• ë‹¹í•  ìˆ˜ë„ ìˆìŠµë‹ˆë‹¤.
-        // í•˜ì§€ë§Œ FindObjectOfTypeì€ ë„ˆë¬´ ë¹„íš¨ìœ¨ì ì´ê¸´ í•˜ë‹¤
+        // playerAtk ì»´í¬?ŒíŠ¸ë¥??ë™?¼ë¡œ ì°¾ì•„??? ë‹¹???˜ë„ ?ˆìŠµ?ˆë‹¤.
+        // ?˜ì?ë§?FindObjectOfType?€ ?ˆë¬´ ë¹„íš¨?¨ì ?´ê¸´ ?˜ë‹¤
         //playerAtk = FindObjectOfType<PlayerAtk>();
->>>>>>> 490b48c1d07f9272897a1d5bb968027958be33a4
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,11 +18,6 @@ public class WeaponColliderScript : MonoBehaviour
             Rigidbody enemyRb = other.GetComponent<Rigidbody>();
             if (enemyRb != null)
             {
-<<<<<<< HEAD
-                // Player°¡ ¹Ù¶óº¸´Â ¹æÇâÀ¸·Î ÈûÀ» °¡ÇÔ
-=======
-                // Playerê°€ ë°”ë¼ë³´ëŠ” ë°©í–¥ìœ¼ë¡œ í˜ì„ ê°€í•¨
->>>>>>> 490b48c1d07f9272897a1d5bb968027958be33a4
                 Vector3 forceDirection = playerAtk.player.transform.forward;
                 enemyRb.AddForce(forceDirection.normalized * playerAtk.forceMagnitude, ForceMode.Impulse);
             }
