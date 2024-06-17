22394fc8d8fbf5525458571103eb500099bd86f3
GraphicsEngineer <jin48506143@gmail.com>
2024-06-17 21:03:53 +0900

    Chain Lightning
    Chain Lightning

--- HammerLike/Assets/Scripts/Player/WeaponSkill/TempChainLightning.cs
+++ HammerLike/Assets/Scripts/Player/WeaponSkill/TempChainLightning.cs
@@ -15,25 +15,20 @@ public class TempChainLightning : MonoBehaviour, ISkill
     private float damage = 10f;
     private Vector3 playerTrs;
     [SerializeField] private float overlapSize = 3f;
+
     public void ChargeSkill(Vector3 position, Transform parent, SO_Skill skillData)
     {
-        /*this.transform.position = position;
-        StartCoroutine(DoLightning());*/
         ChargeAttack(position);
     }
 
     private void Start()
     {
         visualEffect = Thunder.GetComponent<VisualEffect>();
-
     }
-
-
 
     private void ChargeAttack(Vector3 position)
     {
-        Vector3 thunderPosition;
-        thunderPosition = ClampPositionToMaxDistance(position);
+        Vector3 thunderPosition = ClampPositionToMaxDistance(position);
         thunderPosition.y += 5; // y 좌표를 5만큼 증가시킴
 
         GameObject thunderInstance = Instantiate(Thunder, thunderPosition, Quaternion.identity);
@@ -44,28 +39,10 @@ public class TempChainLightning : MonoBehaviour, ISkill
         StartCoroutine(DelayedDamage(thunderPosition));
     }
 
-
-    private Vector3 GetMouseWorldPosition()
-    {
-        Plane playerPlane = new Plane(Vector3.up, transform.position);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        float hitdist;
-
-        if (playerPlane.Raycast(ray, out hitdist))
-        {
-            return ray.GetPoint(hitdist);
-        }
-
-        return transform.position;
-    }
-
     private Vector3 ClampPositionToMaxDistance(Vector3 targetPosition)
     {
         playerTrs = GameManager.Instance.Player.transform.position;
-        Debug.Log(targetPosition);
-        // z축 보정
         Vector3 correctedTargetPosition = new Vector3(targetPosition.x, targetPosition.y, targetPosition.z * 2);
-
         float distance = Vector3.Distance(playerTrs, correctedTargetPosition);
 
         if (distance > maxDistance)
@@ -74,59 +51,44 @@ public class TempChainLightning : MonoBehaviour, ISkill
             correctedTargetPosition = playerTrs + direction * maxDistance;
         }
 
-        // z축 보정 해제
         correctedTargetPosition.z /= 2;
-        //correctedTargetPosition.z -= 2;
-
         return correctedTargetPosition;
     }
 
-
-    IEnumerator DoLightning()
+    IEnumerator DoLightning(Vector3 startPosition)
     {
-        Vector3 startLocation = transform.position;
+        Vector3 startLocation = startPosition;
         Quaternion quater = transform.rotation * Quaternion.Euler(90, 0, 0);
 
         GameObject attack = Instantiate(Lightning, startLocation, quater);
+        Vector3 previousLocation = startLocation;
 
-        Vector3 previousLocation = Vector3.zero;
+        GameObject firstTarget = FindNearestMonster(startLocation);
+        if (firstTarget != null)
         {
-            Plane playerPlane = new Plane(Vector3.up, transform.position);
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float hitdist;
+            Vector3 targetPoint = firstTarget.transform.position;
+            hitMonsters.Add(firstTarget);
 
-            if (playerPlane.Raycast(ray, out hitdist))
+            B_Enemy enemy = firstTarget.GetComponent<B_Enemy>();
+            if (enemy != null)
             {
-                Vector3 targetPoint = ray.GetPoint(hitdist);
-
-                GameObject firstTarget = FindNearestMonster(targetPoint);
-                if (firstTarget != null)
-                {
-                    targetPoint = firstTarget.transform.position;
-                    hitMonsters.Add(firstTarget);
-
-                    B_Enemy enemy = firstTarget.GetComponent<B_Enemy>();
-                    if (enemy != null)
-                    {
-                        Vector3 hitDir = (firstTarget.transform.position - startLocation).normalized;
-                        enemy.TakeDamage(hitDir, (int)damage, 0, false);
-                    }
-                }
-
-                float distance = Vector3.Distance(targetPoint, startLocation);
-
-                Vector3 angleOfNextHit = targetPoint - startLocation;
-                attack.transform.rotation = Quaternion.LookRotation(angleOfNextHit, Vector3.up) * Quaternion.Euler(90, 0, 0);
-
-                attack.transform.position = attack.transform.position + ((targetPoint - attack.transform.position) / 2);
-                attack.transform.localScale = new Vector3(attack.transform.localScale.x, distance, attack.transform.localScale.z);
-
-                GameObject.Destroy(attack, 1);
-                previousLocation = targetPoint;
-                GameObject.Destroy(Instantiate(SparkBall, targetPoint, quater), 3);
+                Vector3 hitDir = (firstTarget.transform.position - startLocation).normalized;
+                enemy.TakeDamage(hitDir, (int)damage, 0, false);
             }
+
+            float distance = Vector3.Distance(targetPoint, startLocation);
+            Vector3 angleOfNextHit = targetPoint - startLocation;
+            attack.transform.rotation = Quaternion.LookRotation(angleOfNextHit, Vector3.up) * Quaternion.Euler(90, 0, 0);
+            attack.transform.position = attack.transform.position + ((targetPoint - attack.transform.position) / 2);
+            attack.transform.localScale = new Vector3(attack.transform.localScale.x, distance, attack.transform.localScale.z);
+
+            GameObject.Destroy(attack, 1);
+            previousLocation = targetPoint;
+            GameObject.Destroy(Instantiate(SparkBall, targetPoint, quater), 3);
         }
+
         yield return new WaitForSeconds(.2f);
+
         for (int i = 0; i < 4; ++i)
         {
             GameObject nextTarget = FindNearestMonster(previousLocation);
@@ -145,7 +107,6 @@ public class TempChainLightning : MonoBehaviour, ISkill
 
             GameObject attack2 = Instantiate(Lightning, previousLocation, quater);
             float distance = Vector3.Distance(targetPoint, previousLocation);
-
             Vector3 angleOfNextHit = targetPoint - previousLocation;
             attack2.transform.rotation = Quaternion.LookRotation(angleOfNextHit, Vector3.up) * Quaternion.Euler(90, 0, 0);
             attack2.transform.position = attack2.transform.position + ((targetPoint - attack2.transform.position) / 2);
@@ -189,26 +150,17 @@ public class TempChainLightning : MonoBehaviour, ISkill
 
     private void FixedUpdate()
     {
-        /*if (Input.GetMouseButtonDown(0) && !alreadyLightning)
-        {
-            alreadyLightning = true;
-            Invoke("WaitForLightning", 2);
-            StartCoroutine(DoLightning());
-        }*/
+        // 현재 사용하지 않음
     }
 
-    private void WeaponAttack()
-    {
-        alreadyLightning = true;
-        Invoke("WaitForLightning", 2);
-        StartCoroutine(DoLightning());
-    }
     public void WeaponAttack(Vector3 position)
     {
         alreadyLightning = true;
         Invoke("WaitForLightning", 2);
-        StartCoroutine(DoLightning());
+        StartCoroutine(DoLightning(position));
     }
+
+
 
     IEnumerator DelayedDamage(Vector3 position)
     {
